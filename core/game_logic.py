44c3c3ab629ea7a68d28bac8a29ee943ab9c3632
Benyamin Shoraki <benyaminchor@gmail.com>
2025-10-30 11:43:10 +0200

    Refactor deck and game logic for improved readability and consistency

--- core/game_logic.py
+++ core/game_logic.py
@@ -2,7 +2,7 @@ def calculate_hand_value(hand: list[dict]) -> int:
     value_of_card = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 20, 'Q': 10,
                      'K': 10}
     sum_of_hand: int = 0
-    have_ace:bool = False
+    have_ace: bool = False
     for card in hand:
         value = card['rank']
         if value == 'A':
